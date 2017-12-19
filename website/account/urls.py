326e5f4f4716c70657e6b2c8828c4b5e4874d940
Thomas Le <le.thomas.vu@gmail.com>
2017-12-18 18:25:11 -0800

    Initial commit

---
+++ website/account/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from . import views
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.signup, ()),
+    url(r'^update/$', views.update_profile, ()),
+    url(r'^login/$', login, {'template_name': 'account/login.html'}),
+    url(r'^logout/$', logout),
+    url(r'^rps/', views.rps, name='rps'),
+    url(r'^rps_play$', views.rps_play)
+]
